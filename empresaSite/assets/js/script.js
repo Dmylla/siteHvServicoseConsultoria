790b0326b1e67406b858ecbe39df16586440e747
Edivaldo Junior <148983172+JuniorAvelar@users.noreply.github.com>
2025-07-12 20:48:58 -0300

    style: adicionar efeitos de trasiçoes

--- empresaSite/assets/js/script.js
+++ empresaSite/assets/js/script.js
@@ -26,3 +26,97 @@ contatoform.addEventListener("submit" , (e) => {
 
     window.open(url)
 })
+
+
+// gsap
+
+gsap.registerPlugin(ScrollTrigger);
+
+// hero section
+gsap.fromTo(".hero-title", {
+    x:-1000,
+    opacity:0
+} , {
+    x:0,
+    duration:2,
+    opacity:1,
+})
+
+gsap.fromTo(".description-hero", {
+    x:-1000,
+    opacity:0
+} , {
+    x:0,
+    duration:2.5,
+    opacity:1
+})
+
+gsap.fromTo(".botaoContatoDois", {
+    x:-1000,
+    opacity:0
+} , {
+    x:0,
+    duration:3,
+    opacity:1
+})
+
+// sobre-serction
+gsap.fromTo(".tituloSobrenois" ,{
+    y:500,
+    opacity:0
+
+},{
+    y:0,
+    duration:2,
+    opacity:1,
+     scrollTrigger:{
+        trigger:".hero-title",
+        start:"top 15%",
+        toggleActions:"play none none none"
+    }
+})
+
+gsap.fromTo(".sobrenoisparagrafo" , {
+    y: 500,
+    opacity:0,
+},{
+    y:0,
+    duration:2,
+    opacity:1,
+    scrollTrigger: {
+        trigger:".sobrenoisparagrafo",
+        start:"top 160%",
+        toggleActions:"play none none none"
+    }
+})
+
+gsap.fromTo(".imgsobrenois", {
+    y:500,
+    opacity:0
+},{
+    y:0,
+    duration:2,
+    opacity:1,
+    scrollTrigger: {
+        trigger:".imgsobrenois",
+        start: "top 160%",
+        toggleActions:"play none none none"
+    }
+})
+
+// teste
+
+    gsap.fromTo(".cardes" , {
+        x: -2000,
+        opacity:0
+        },{
+        x:0,
+        opacity:1,
+        duration: 2,
+        scrollTrigger:{
+            trigger:".cardes",
+            start:"top 90%",
+            toggleActions:"play none none none"
+        }
+    })
+
