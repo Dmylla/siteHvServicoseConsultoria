74b159a7260312468a0669c230d462a3a0225ea9
Edivaldo Junior <148983172+JuniorAvelar@users.noreply.github.com>
2025-07-13 16:46:10 -0300

    refactore: Separa o codigo das animações do gsap e da manipulação do Dom em pastas diferentes

--- empresaSite/assets/js/script.js
+++ empresaSite/assets/js/script.js
@@ -28,125 +28,30 @@ contatoform.addEventListener("submit" , (e) => {
 })
 
 
-// gsap
+// More serviços
 
-gsap.registerPlugin(ScrollTrigger);
+const buttonMoreServices = document.querySelector(".serviços-button")
 
-// hero section
-gsap.fromTo(".hero-title", {
-    x:-1000,
-    opacity:0
-} , {
-    x:0,
-    duration:2,
-    opacity:1,
-})
+console.log(buttonMoreServices)
+buttonMoreServices.addEventListener("click" , () => {
 
-gsap.fromTo(".description-hero", {
-    x:-1000,
-    opacity:0
-} , {
-    x:0,
-    duration:2.5,
-    opacity:1
-})
-
-gsap.fromTo(".botaoContatoDois", {
-    x:-1000,
-    opacity:0
-} , {
-    x:0,
-    duration:3,
-    opacity:1
-})
-
-// sobre-serction
-gsap.fromTo(".tituloSobrenois" ,{
-    y:500,
-    opacity:0
-
-},{
-    y:0,
-    duration:2,
-    opacity:1,
-     scrollTrigger:{
-        trigger:".hero-title",
-        start:"top 15%",
-        toggleActions:"play none none none"
-    }
-})
-
-gsap.fromTo(".sobrenoisparagrafo" , {
-    y: 500,
-    opacity:0,
-},{
-    y:0,
-    duration:2,
-    opacity:1,
-    scrollTrigger: {
-        trigger:".sobrenoisparagrafo",
-        start:"top 160%",
-        toggleActions:"play none none none"
-    }
-})
-
-gsap.fromTo(".imgsobrenois", {
-    y:500,
-    opacity:0
-},{
-    y:0,
-    duration:2,
-    opacity:1,
-    scrollTrigger: {
-        trigger:".imgsobrenois",
-        start: "top 160%",
-        toggleActions:"play none none none"
-    }
-})
-
-
-    gsap.fromTo(".cardes" , {
-        x: -2000,
-        opacity:0
-        },{
-        x:0,
-        opacity:1,
-        duration: 2,
-        scrollTrigger:{
-            trigger:".cardes",
-            start:"top 90%",
-            toggleActions:"play none none none"
-        }
-    })
-
-
-    // serviçoes section
-
-    gsap.fromTo(".tituloNossosServicos" , {
-        x:-1000,
-        opacity:0
-    } , {
-        x:0,
-        opacity:1,
-        duration:2,
-        scrollTrigger:{
-            trigger:".tituloNossosServicos",
-            start:"top 90%",
-            toggleActions:"play none none none"
-        }
-    })
-
-
-    gsap.fromTo(".itenServiço" , {
-        y:500,
-        opacity:0
+    gsap.fromTo(".more-services",{
+        y:300,
+        opacity:0,
+        display:"none"
     },{
         y:0,
-        duration:2,
         opacity:1,
-        scrollTrigger:{
-            trigger:".itenServiço",
-            start:"top 150%",
-            toggleActions:"play none none none"
-        }
+        duration:1  ,
+        display:"flex"
     })
+
+
+    // console.log
+    // const divMoreServices = document.querySelector(".more-services")
+    // divMoreServices.style.display = "flex"
+    // divMoreServices.style.opacity = "1"
+    // console.log(divMoreServices)
+})
+
+
